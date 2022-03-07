82a06b8df5a335c43bb486a81969b2ed673d1f79
Regisson Cesar <regisson.aguiar@liferay.com>
2022-03-07 17:20:23 -0300

    index.js modified

--- src/index.js
+++ src/index.js
@@ -1,23 +1,32 @@
-
+import { tab } from '@testing-library/user-event/dist/tab';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 class Header extends React.Component {
+
   constructor(props) {
     super(props);
-    this.state = {favoritecolor: "red"};
+    this.state = {favoritecolor: "  "};
   }
   componentDidMount() {
     setTimeout(() => {
       this.setState({favoritecolor: "yellow"})
-    }, 3000)
+    }, 1000)
+  }
+  componentDidUpdate() {
+    document.getElementById("mydiv").innerHTML =
+    "The updated favorite is " + this.state.favoritecolor;
   }
   render() {
     return (
+      <div>
       <h1>My Favorite Color is {this.state.favoritecolor}</h1>
+      <div id="mydiv"></div>
+      </div>
     );
   }
 }
 
 ReactDOM.render(<Header />, document.getElementById('root'));
+
 
